273ee74a141382d5ff91a3aa3290ecb5994dc32a
Baebae <baebae@gmail.com>
2016-02-11 04:34:08 +0300

    fix 0.19 issue

--- android/src/main/java/com/baebae/reactnativecamera/CameraViewManager.java
+++ android/src/main/java/com/baebae/reactnativecamera/CameraViewManager.java
@@ -6,7 +6,7 @@ import android.app.Activity;
 import android.support.annotation.Nullable;
 import android.view.View;
 
-import com.facebook.react.uimanager.ReactProp;
+import com.facebook.react.uimanager.annotations.ReactProp;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewGroupManager;
