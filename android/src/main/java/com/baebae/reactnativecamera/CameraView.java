073fcf7a1103f5cad7bb445123712a8395b7842a
Andrew Crowell <andrewj.crowell@gmail.com>
2016-03-18 14:40:13 -0700

    Disable landscape again.

--- android/src/main/java/com/baebae/reactnativecamera/CameraView.java
+++ android/src/main/java/com/baebae/reactnativecamera/CameraView.java
@@ -51,7 +51,7 @@ public class CameraView extends CameraPreviewLayout implements LifecycleEventLis
 
             @Override
             public void onSensorChanged(SensorEvent event) {
-                float[] values = event.values;
+                /*float[] values = event.values;
                 int orientation = ORIENTATION_UNKNOWN;
                 float X = -values[_DATA_X];
                 float Y = -values[_DATA_Y];
@@ -113,7 +113,7 @@ public class CameraView extends CameraPreviewLayout implements LifecycleEventLis
             public void onAccuracyChanged(Sensor sensor, int accuracy) {
                 // TODO Auto-generated method stub
 
-            }
+            }*/
         };
         sensorManager.registerListener(orientationListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_GAME);
     }
